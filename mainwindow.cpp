f0b747345164fd0ad455f737a67f843695989ff1
AlexP2003 <115130190+AlexP2003@users.noreply.github.com>
2022-11-14 04:21:11 -0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    
+    QPixmap pix("C:/Users/alexp/Desktop/Git and QT/Fall2022-CS3A-QT-Project1/imgs/Left graphic.jpg");
+    int w = ui->graphicsView->width();
+    int h = ui->graphicsView->height();
+    ui->graphicsView->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+
+    QPixmap pix("C:/Users/alexp/Desktop/Git and QT/Fall2022-CS3A-QT-Project1/imgs/Right graphic.jpg");
+    int w = ui->graphicsView_2->width();
+    int h = ui->graphicsView_2->height();
+    ui->graphicsView_2->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
